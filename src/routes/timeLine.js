ba80434450cdab5e78f96b88e15db67f7cc8b889
Zarahi93 <107142458+Zarahi93@users.noreply.github.com>
2022-09-09 13:55:57 -0500

    .

--- src/routes/timeLine.js
+++ src/routes/timeLine.js
@@ -1,21 +1,19 @@
- import { root } from "../main"
- export const timeLine = function () {
-    const timeLineContainer = document.createElement("section") /* contenedor principal de los otros dos divs*/
-    const postUserContainer = document.createElement("article")
-    const inputText = document.createElement("input")
-    const button = document.createElement("button")
-    const postsContainer= document.createElement("article") 
-    const userPicture = document.createElement("img")
-    const userName= document.createElement("h4")
-    inputText.type= "text";
-    buttton.textContent="publicar"
-    root.append(timeLineContainer)
-    timeLineContainer.append(postUserContaine, postsContainer)
-    postUserContainer.append(inputText, button)
-    postsCointainer.append(userPicture, userName)
+import { root } from '../main';
 
-     
+export const timeLine = function () {
+  const timeLineContainer = document.createElement('section'); /* contenedor principal de los otros dos divs */
+  const postUserContainer = document.createElement('article');
+  const inputText = document.createElement('input');
+  const button = document.createElement('button');
+  const postsContainer = document.createElement('article');
+  const userPicture = document.createElement('img');
+  const userName = document.createElement('h4');
+  inputText.type = 'text';
+  button.textContent = 'publicar';
+  root.append(timeLineContainer);
+  timeLineContainer.append(postUserContaine, postsContainer);
+  postUserContainer.append(inputText, button);
+  postsCointainer.append(userPicture, userName);
 
-return mainContainer
-
-}
+  return mainContainer;
+};
