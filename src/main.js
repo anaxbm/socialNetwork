b09be48790c74392e08a58f05da54296da467acf
anankebj <anankebj@gmail.com>
2022-09-13 10:05:15 -0500

    addingStyleTimeLineRoute

--- src/main.js
+++ src/main.js
@@ -1,28 +1,31 @@
 // import pages
-import {timeLine} from './routes/timeLine.js';
-import {login} from './routes/login.js';
+import { timeLine } from "./routes/timeLine.js";
+import { login } from "./routes/login.js";
 
-//Connect with html
- export const root = document.getElementById('root');//In this node is where everything is render
+// Connect with html
+export const root = document.getElementById("root"); // In this node is where everything is render
+const routes = {
+  // Object that contains the routes and what to render
+  "/": login,
+  "/timeLine": timeLine,
+};
 
-const routes = { //Object that contains the routes and what to render
-    '/': login,
-    '/timeLine': timeLine, 
-  };
-  export const onNavigate = (pathname) => {//Takes pathname and render section according to it
-    window.history.pushState(
-      {}, //State
-      pathname, //Title
-      window.location.origin + pathname,//Domian or url
-    );
-    root.removeChild(root.firstChild);//
-    root.appendChild(routes[pathname]());//Run the function to enter the pathname received
-  };
-  const pages = routes[window.location.pathname];//Render to "/"
+export const onNavigate = (pathname) => {
+  // Takes pathname and render section according to it
+  window.history.pushState(
+    {}, // State
+    pathname, // Title
+    window.location.origin + pathname // Domian or url
+  );
+  root.removeChild(root.firstChild); //
+  root.appendChild(routes[pathname]()); // Run the function to enter the pathname received
+};
 
-  window.onpopstate = () => {
-    root.removeChild(root.firstChild);
-    root.append(pages());
-  };
+const pages = routes[window.location.pathname]; // Render to "/"
 
-  root.appendChild(pages());
+window.onpopstate = () => {
+  root.removeChild(root.firstChild);
+  root.append(pages());
+};
+
+root.appendChild(pages());
