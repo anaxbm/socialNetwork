cbacbb96b5df371e26412c4c250c428cd9642180
Zarahi93 <107142458+Zarahi93@users.noreply.github.com>
2022-09-12 20:52:28 -0500

    Final draft

--- src/routes/register.js
+++ src/routes/register.js
@@ -1,35 +1,40 @@
 import { onNavigate } from '../main.js';
 
 export const register = () => {
-  const sectionEmail = document.createElement('section');
-  sectionEmail.setAttribute('class', 'registerEmail');
-  const sectionPass = document.createElement('sectionPass');
-  sectionPass.setAttribute('class', 'register');
-  const sectionButton = document.createElement('section');
-  sectionButton.setAttribute('class', 'registerButton');
+  const sectionInput = document.createElement('section');
+  sectionInput.setAttribute('class', 'sectionInput');
+  const sectionLine = document.createElement('section');
+  sectionLine.setAttribute('class', 'sectionLine');
   const sectionAll = document.createElement('section');
   sectionAll.setAttribute('class', 'register');
 
+  const registerButton = document.createElement('button');
+  registerButton.setAttribute('class', 'registerButton');
+  const googleButton = document.createElement('button');
+  googleButton.setAttribute('class', 'googleButton');
+
   const title = document.createElement('h2');
   title.setAttribute('class', 'title');
-  const registerButton = document.createElement('button');
-  const googleButton = document.createElement('button');
-  const inputEmail = document.createElement('input');
+  const email = document.createElement('input');
   const emailOne = document.createElement('p');
   const confirmEmail = document.createElement('input');
   const emailTwo = document.createElement('p');
-  const inputPass = document.createElement('input');
+  const pass = document.createElement('input');
   const passOne = document.createElement('p');
   const confirmPass = document.createElement('input');
   const passTwo = document.createElement('p');
+  const hrone = document.createElement('hr');
+  const leter = document.createElement('p');
+  const hrtwo = document.createElement('hr');
 
+  title.textContent = 'Animal Pawnet';
   registerButton.textContent = 'Registrarse';
   googleButton.textContent = 'continuar con Google';
-  title.textContent = 'Animal Pawnet';
-  emailOne.textContent = 'Correo electrónico';
-  emailTwo.textContent = 'Confirmar correo electrónico';
+  emailOne.textContent = 'Email';
+  emailTwo.textContent = 'Confirmar Email';
   passOne.textContent = 'Contraseña';
   passTwo.textContent = 'Confirmar contraseña';
+  leter.textContent = 'ó';
 
   registerButton.addEventListener('click', () => {
     onNavigate('/');
@@ -37,10 +42,9 @@ export const register = () => {
   googleButton.addEventListener('click', () => {
   });
 
-  sectionEmail.append(title, inputEmail, emailOne, confirmEmail, emailTwo);
-  sectionPass.append(inputPass, passOne, confirmPass, passTwo);
-  sectionButton.append(registerButton, googleButton);
-  sectionAll.append(sectionEmail, sectionPass, sectionButton);
+  sectionInput.append(email, emailOne, confirmEmail, emailTwo, pass, passOne, confirmPass, passTwo);
+  sectionLine.append(hrone, leter, hrtwo);
+  sectionAll.append(title, sectionInput, registerButton, sectionLine, googleButton);
 
   return sectionAll;
 };
