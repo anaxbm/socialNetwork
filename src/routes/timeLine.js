c812bb37478b0fa6d86a19e7ce09da2f81a649f9
anankebj <anankebj@gmail.com>
2022-09-13 23:26:46 -0500

    Revert "Merge branch 'draft' into develop"
    This reverts commit aa9ed2827f4bcc5eb1687e11d6ff4c18ac8f199b, reversing
    changes made to d94ad35c59689a76f0ed7292121ce0c187353cda.

--- src/routes/timeLine.js
+++ src/routes/timeLine.js
@@ -1,4 +1,4 @@
-//Creating elements
+// Creating elements
 export const timeLine = () => {
   // contenedor principal de los otros dos divs*/
   const timeLineMainContainer = document.createElement("section");
@@ -103,4 +103,3 @@ export const timeLine = () => {
 
   return timeLineMainContainer;
 };
-
