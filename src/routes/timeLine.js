d94ad35c59689a76f0ed7292121ce0c187353cda
anankebj <anankebj@gmail.com>
2022-09-13 21:11:51 -0500

    TimeLine UI done.
    I added styles to timeLine route and responsive design.

--- src/routes/timeLine.js
+++ src/routes/timeLine.js
@@ -1,34 +1,61 @@
-//Creating elements
+// Creating elements
 export const timeLine = () => {
-  /* contenedor principal de los otros dos divs*/
-  const timeLineContainer = document.createElement("section");
+  // contenedor principal de los otros dos divs*/
+  const timeLineMainContainer = document.createElement("section");
+  const timeLineContainer = document.createElement("article");
+
   const postUserContainer = document.createElement("article");
+  const inputWrapper = document.createElement("div");
   const inputText = document.createElement("input");
   const postingButton = document.createElement("button");
-  const postsContainer = document.createElement("article");
-  const userPicture = document.createElement("img");
-  /*const UserPostPicture = document.createElement("img");*/
-  const userName = document.createElement("h4");
-  const userContainer = document.createElement("div");
+  const userPostPicture = document.createElement("img");
+
+  const feed = document.createElement("div");
+
   const userNameContainer = document.createElement("div");
+  const userName = document.createElement("h4");
+  const userPicture = document.createElement("img");
+
   const contentPostContainer = document.createElement("div");
   const iconsContainer = document.createElement("div");
+
+  const navBarItems_1 = document.createElement("div");
+  const navBarItems_2 = document.createElement("div");
+  const navBarItems_3 = document.createElement("div");
+  const navBarText_1 = document.createElement("p");
+  const navBarText_2 = document.createElement("p");
+  const navBarText_3 = document.createElement("p");
+  navBarItems_1.classList.add("iconsNavBar");
+  navBarItems_2.classList.add("iconsNavBar");
+  navBarItems_1.classList.add("iconsNavBar");
+
   const menuBarIconsContainer = document.createElement("div");
 
-  /*adding classes*/
-  /*dentro de maintimeline debe estar*/
+  /* adding classes */
+  /* dentro de maintimeline debe estar */
+  timeLineMainContainer.classList.add("timeLineMainContainer");
   timeLineContainer.classList.add("mainTimeline-Container");
   postUserContainer.classList.add("createPostTLContainer");
+  inputWrapper.classList.add("inputWrapper");
   inputText.classList.add("inputText");
   postingButton.classList.add("postButton");
-  userContainer.classList.add("userContainer");
-  userNameContainer.classList.add("userNameContainer");
-  userPicture.classList.add("userPicture");
-  contentPostContainer.classList.add("contentPostContainer");
-  iconsContainer.classList.add("iconsContainer"); //same class to apply same height
-  menuBarIconsContainer.classList.add("iconsContainer");
 
-  /*creating  sub-containers for bar and icons to like and edit*/
+  feed.classList.add("feedContainer");
+  userNameContainer.classList.add("userNameContainer");
+  userName.classList.add("userName");
+  userPicture.classList.add("userPicture");
+  userPostPicture.classList.add("userPostPicture");
+  contentPostContainer.classList.add("contentPostContainer");
+  iconsContainer.classList.add("iconsContainer"); // same class to apply same height
+  navBarText_1.classList.add("navBarTex");
+  navBarText_2.classList.add("navBarTex");
+  navBarText_3.classList.add("navBarTex");
+  navBarText_1.textContent = "Busca un hogar";
+  navBarText_2.textContent = "Adoptar";
+  navBarText_3.textContent = "Mi cuenta";
+  menuBarIconsContainer.classList.add("navBar");
+
+  /* creating  sub-containers for bar and icons to like and edit */
   const iconsImages_1 = document.createElement("img");
   const iconsImages_2 = document.createElement("img");
   const iconsImages_3 = document.createElement("img");
@@ -45,7 +72,7 @@ export const timeLine = () => {
   inputText.type = "text";
   postingButton.textContent = "publicar";
   userPicture.src = "./images/usuario.png";
-  UserPostPicture.src = "./images/usuario.png";
+  userPostPicture.src = "./images/usuario.png";
   userName.textContent = "Adahi Gallardo";
   iconsImages_1.src = "./images/bin.png";
   iconsImages_2.src = "./images/edit.png";
@@ -56,27 +83,23 @@ export const timeLine = () => {
 
   /* displays input text and button */
 
-  postUserContainer.append(inputText, postingButton);
+  inputWrapper.append(inputText, postingButton);
+  postUserContainer.append(userPostPicture, inputWrapper);
   userNameContainer.append(userPicture, userName);
-  userContainer.append(
+  feed.append(
     postUserContainer,
     userNameContainer,
-    contentPostContainer
+    contentPostContainer,
+    iconsContainer
   );
   iconsContainer.append(iconsImages_1, iconsImages_2, iconsImages_3);
-  menuBarIconsContainer.append(
-    menuBarImages_1,
-    menuBarImages_2,
-    menuBarImages_3
-  );
 
-  timeLineContainer.append(
-    postUserContainer,
-    postsContainer,
-    userContainer,
-    iconsContainer,
-    menuBarIconsContainer
-  );
+  navBarItems_1.append(menuBarImages_1, navBarText_1);
+  navBarItems_2.append(menuBarImages_2, navBarText_2);
+  navBarItems_3.append(menuBarImages_3, navBarText_3);
+  menuBarIconsContainer.append(navBarItems_1, navBarItems_2, navBarItems_3);
+  timeLineContainer.append(postUserContainer, feed, menuBarIconsContainer);
+  timeLineMainContainer.append(timeLineContainer);
 
-  return timeLineContainer;
+  return timeLineMainContainer;
 };
