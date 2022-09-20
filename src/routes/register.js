29a364bdee5c0303fd46ba8c81ed24811c5cf218
anankebj <anankebj@gmail.com>
2022-09-20 12:05:32 -0500

    "changing message error in register"
    Hello team, here we started working with errors with promises in register route.. still pending...

--- src/routes/register.js
+++ src/routes/register.js
@@ -1,7 +1,6 @@
 import { onNavigate } from '../main.js';
 
-import { addUser, googleProvider} from '../lib/auth.js';
-
+import { addUser, googleProvider } from '../lib/auth.js';
 
 export const register = () => {
   const mainRegister = document.createElement('main');
@@ -12,7 +11,6 @@ export const register = () => {
   sectionLine.setAttribute('class', 'sectionLine');
   const sectionAll = document.createElement('section');
   sectionAll.setAttribute('class', 'sectionRegister');
-
   const registerButton = document.createElement('button');
   registerButton.setAttribute('class', 'registerButton');
   const googleButton = document.createElement('button');
@@ -36,6 +34,7 @@ export const register = () => {
   leter.setAttribute('id', 'leter');
   const hrTwo = document.createElement('hr');
   hrTwo.setAttribute('class', 'hr');
+  const divMessage = document.createElement('div');
 
   title.textContent = 'Animal Pawnet';
   registerButton.textContent = 'Registrate';
@@ -49,9 +48,11 @@ export const register = () => {
     addUser(userEmail, userPassword)
       .then((userCredential) => {
         onNavigate('/timeline');
-      }).catch((error) => {
+      })
+      .catch((error) => {
         const errorCode = error.code; // auth/invalid-email
         const errorMessage = error.message; // Firebase: Error (auth/invalid-email)
+        divMessage.innerHTML = `<p> mal </p> `;
       });
   });
   // End of the declararion of the functions for Google button
@@ -59,12 +60,12 @@ export const register = () => {
     googleProvider()
       .then((userCredential) => {
         onNavigate('/timeline');
-      }).catch((error) => {
-        const errorCode = error.code; // auth/invalid-email
-        const errorMessage = error.message;  
       })
-
-});
+      .catch((error) => {
+        const errorCode = error.code; // auth/invalid-email
+        const errorMessage = error.message;
+      });
+  });
 
   sectionInput.append(email, emailOne, pass, passOne);
   sectionLine.append(hrOne, leter, hrTwo);
